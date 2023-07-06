213a27cb810d1483ad326a4cfacd7940d7f6c24d
Pouya Mirzaei <125562043+PouyaMirzaei@users.noreply.github.com>
2023-07-07 01:36:11 +0330

    Add files via upload

--- binary_search.py
+++ binary_search.py
@@ -1,10 +1,25 @@
-# def naive_search(l,target):
-#     l = [1,3,10,12]
-#     for i in range(len(l)):
-#         if l[i] == target:
-#             return l;
-#     return -l
+# A fucnction that takes a list and target parameter
+# [1,2,3,4,5,6,7,8,9,10]  3:  (0+9)/2   (0+5)/2 :2.5==3   
+# multiple variables : middle , start ,end ,steps
 
-def binary_search(l,target):
-    #example l = [1,3,5,10,12]
-    midpoint
+def binarry_search(list,element):
+    middle =0
+    start = 0
+    end = len(list)
+    steps = 0
+    
+    while(start<=end):
+        print("steps",steps, ":" ,str(list[start:end+1]))
+        steps +=1
+        middle = (start + end) //2
+        if element == list[middle]:
+            return middle
+        if element < list[middle]:
+            end = middle -1
+        else:
+            start = middle +1
+            
+    return -1
+my_list = [1,2,3,4,5,6,7,8,9]
+target = 1
+binarry_search(my_list,target)
