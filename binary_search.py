63a78f2840aa57d211404dd4d722585ed93ccf19
Pouya Mirzaei <125562043+PouyaMirzaei@users.noreply.github.com>
2023-07-07 00:50:38 +0330

    Add files via upload

---
+++ binary_search.py
@@ -0,0 +1,10 @@
+# def naive_search(l,target):
+#     l = [1,3,10,12]
+#     for i in range(len(l)):
+#         if l[i] == target:
+#             return l;
+#     return -l
+
+def binary_search(l,target):
+    #example l = [1,3,5,10,12]
+    midpoint
